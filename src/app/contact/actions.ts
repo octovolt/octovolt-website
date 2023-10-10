f5164bd7a9884f557c65834b2d5f421c0e7f6b75
octovolt <contact.octovolt@gmail.com>
2023-10-09 22:26:42 -0700

    trying Brevo transactional email service

--- src/app/contact/actions.ts
+++ src/app/contact/actions.ts
@@ -9,19 +9,26 @@ import { redirect } from 'next/navigation';
 export async function sendMessage(formData: FormData) {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
+      host: 'smtp-relay.sendinblue.com',
+      port: 587,
       secure: true,
       auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
+        user: process.env.TRANSACTIONAL_EMAIL_USER,
+        pass: process.env.TRANSACTIONAL_EMAIL_PASSWORD,
       },
     });
     transporter.sendMail({
-      from: process.env.EMAIL,
-      to: process.env.EMAIL,
+      from: process.env.CONTACT_EMAIL_FROM,
+      to: process.env.CONTACT_EMAIL_TO,
       subject: `[CONTACT FORM] ${formData.get('subject')}`,
-      text: `${formData.get('message')}`,
+      text: `
+        Name: ${formData.get('name')}
+        Email: ${formData.get('email')}
+        Subject: ${formData.get('subject')}
+        -------------------------------------
+        Message:
+
+        ${formData.get('message')}`,
     });
 
     redirect(`/contact/success`);
