08aae1067628ffa117ad6f53aeacd51614be0204
octovolt <contact.octovolt@gmail.com>
2023-10-09 23:23:40 -0700

    add promise handling for contact form

--- src/app/contact/actions.ts
+++ src/app/contact/actions.ts
@@ -7,33 +7,34 @@ import nodemailer from 'nodemailer';
 import { redirect } from 'next/navigation';
 
 export async function sendMessage(formData: FormData) {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp-relay.brevo.com',
-      // host: 'smtp-relay.sendinblue.com',
-      port: 587,
-      secure: true,
-      auth: {
-        user: process.env.TRANSACTIONAL_EMAIL_USER,
-        pass: process.env.TRANSACTIONAL_EMAIL_PASSWORD,
-      },
-    });
-    transporter.sendMail({
-      from: process.env.CONTACT_EMAIL_FROM,
-      to: process.env.CONTACT_EMAIL_TO,
-      subject: `[CONTACT FORM] ${formData.get('subject')}`,
-      text: `
-        Name: ${formData.get('name')}
-        Email: ${formData.get('email')}
-        Subject: ${formData.get('subject')}
-        -------------------------------------
-        Message:
+  const transporter = nodemailer.createTransport({
+    host: 'smtp-relay.brevo.com',
+    // host: 'smtp-relay.sendinblue.com',
+    port: 587,
+    secure: true,
+    auth: {
+      user: process.env.TRANSACTIONAL_EMAIL_USER,
+      pass: process.env.TRANSACTIONAL_EMAIL_PASSWORD,
+    },
+  });
+  transporter.sendMail({
+    from: process.env.CONTACT_EMAIL_FROM,
+    to: process.env.CONTACT_EMAIL_TO,
+    subject: `[CONTACT FORM] ${formData.get('subject')}`,
+    text: `
+      Name: ${formData.get('name')}
+      Email: ${formData.get('email')}
+      Subject: ${formData.get('subject')}
+      -------------------------------------
+      Message:
 
-        ${formData.get('message')}`,
-    });
-
+      ${formData.get('message')}`,
+  }, (error, info) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+      redirect(`/contact/error`);
+    }
+    console.log(`Message sent: ${info}`);
     redirect(`/contact/success`);
-  } catch (_error) {
-    redirect(`/contact/error`);
-  }
+  });
 }
